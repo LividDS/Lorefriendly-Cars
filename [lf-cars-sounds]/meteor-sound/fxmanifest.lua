ac39b1515ae446903fb256241790abe8ada3a075
LividDS <113475417+LividDS@users.noreply.github.com>
2023-01-31 21:30:42 +0100

    added meteor & sentinelsg4

---
+++ [lf-cars-sounds]/meteor-sound/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+files {
+  'audioconfig/*.dat151.rel',
+  'audioconfig/*.dat54.rel',
+  'audioconfig/*.dat10.rel',
+  'sfx/**/*.awc'
+}
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/meteor_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/meteor_sounds.dat'
+data_file 'AUDIO_AMPDATA' 'audioconfig/meteor_amp.dat
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_meteor'
---
+++ [lf-cars-sounds]/sentinelsg4-sound/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+files {
+  'audioconfig/*.dat151.rel',
+  'audioconfig/*.dat54.rel',
+  'sfx/**/*.awc'
+}
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/sentinelsg4_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/sentinelsg4_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_sentinelsg4'
