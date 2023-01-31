2ea9ea29e5e650d190641ee26f14727972715cde
LividDS <113475417+LividDS@users.noreply.github.com>
2023-01-31 17:15:07 +0100

    arias & executioner added

---
+++ [lf-cars-sounds]/arias-sound/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+files {
+  'audioconfig/*.dat151.rel',
+  'audioconfig/*.dat54.rel',
+  'sfx/**/*.awc'
+}
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/arias_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/arias_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_stewarias'
---
+++ [lf-cars-sounds]/executioner-sound/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+files {
+  'audioconfig/*.dat151.rel',
+  'audioconfig/*.dat54.rel',
+}
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/executioner_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/executioner_sounds.dat'
