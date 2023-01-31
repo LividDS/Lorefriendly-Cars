54e921ca1b8a76066190716f1d3201cc9adf9d0c
LividDS <113475417+LividDS@users.noreply.github.com>
2023-01-31 21:23:17 +0100

    euros added

---
+++ [lf-cars-sounds]/euros-sound/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+files {
+  'audioconfig/*.dat151.rel',
+  'audioconfig/*.dat54.rel',
+  'sfx/**/*.awc'
+}
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/euros_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/euros_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_trumpetzr'
