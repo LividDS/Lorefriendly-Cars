7fe78f15a58bd1b90b0239da4e2b62c66cc7ebfa
LividDS <113475417+LividDS@users.noreply.github.com>
2023-03-28 16:36:06 +0200

    Vulcar Hachura R added

---
+++ [lf-cars-sounds]/hachura-sound/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+files {
+  'audioconfig/*.dat151.rel',
+  'audioconfig/*.dat54.rel',
+  'audioconfig/*.dat10.rel'
+  'sfx/**/*.awc'
+}
+
+data_file 'AUDIO_AMPDATA' 'audioconfig/hachura_amp.dat
+data_file 'AUDIO_GAMEDATA' 'audioconfig/hachura_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/hachura_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_hachuraelegy'
