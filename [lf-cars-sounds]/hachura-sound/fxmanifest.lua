adc26797a1aeb150c8ebb18ea219a41ff2ea5daf
LividDS <113475417+LividDS@users.noreply.github.com>
2023-03-28 16:37:55 +0200

    Update fxmanifest.lua

--- [lf-cars-sounds]/hachura-sound/fxmanifest.lua
+++ [lf-cars-sounds]/hachura-sound/fxmanifest.lua
@@ -12,4 +12,4 @@ files {
 data_file 'AUDIO_AMPDATA' 'audioconfig/hachura_amp.dat
 data_file 'AUDIO_GAMEDATA' 'audioconfig/hachura_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/hachura_sounds.dat'
-data_file 'AUDIO_WAVEPACK' 'sfx/dlc_hachuraelegy'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_hachura'
