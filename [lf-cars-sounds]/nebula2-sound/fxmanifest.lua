54854c102a6338c24702b7009a10319f6fb02017
LividDS <113475417+LividDS@users.noreply.github.com>
2023-10-15 17:51:45 +0200

    BIG update: 20+ vehicles added
    The BIG update!

---
+++ [lf-cars-sounds]/nebula2-sound/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+files {
+  'audioconfig/*.dat151.rel',
+  'audioconfig/*.dat54.rel',
+  'audioconfig/*.dat10.rel',
+}
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/sbus2_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/sbus2_sounds.dat'
+data_file 'AUDIO_AMPDATA' 'audioconfig/sbus2_amp.dat'
---
+++ [lf-cars-sounds]/sbus2-sound/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+files {
+  'audioconfig/*.dat151.rel',
+  'audioconfig/*.dat54.rel',
+  'audioconfig/*.dat10.rel',
+  'sfx/**/*.awc'
+}
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/sbus2_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/sbus2_sounds.dat'
+data_file 'AUDIO_AMPDATA' 'audioconfig/sbus2_amp.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_sbus2'
