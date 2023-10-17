a58396fa2041433642a59c43793908b97bace0c4
LividDS <113475417+LividDS@users.noreply.github.com>
2023-10-17 10:53:10 +0200

    add: 26 cars added

---
+++ [lf-cars-sounds]/asteropers-sound/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+files {
+  'audioconfig/*.dat151.rel',
+  'audioconfig/*.dat54.rel',
+  'sfx/**/*.awc'
+}
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/asteropers_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/asteropers_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_stewasteropers'
---
+++ [lf-cars-sounds]/brisket-sound/fxmanifest.lua
@@ -0,0 +1,12 @@
+resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+
+files {
+  'brisket_game.dat151.rel',
+  'brisket_sounds.dat54.rel',
+  'sfx/dlc_brisket/brisket.awc',
+  'sfx/dlc_brisket/brisket_npc.awc',
+}
+
+data_file 'AUDIO_GAMEDATA' 'brisket_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'brisket_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_brisket'
