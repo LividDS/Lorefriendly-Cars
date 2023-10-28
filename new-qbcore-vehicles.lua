887ce613a058d34a7f31199b6bed306484a48655
Tweaky <127403786+derTweaky@users.noreply.github.com>
2023-10-28 20:20:39 +0200

    Create new-qbcore-vehicles.lua
    For better adding

---
+++ new-qbcore-vehicles.lua
@@ -0,0 +1,86 @@
+    --- Compacts (0)
+    {['model'] = 'arias',         ['name'] = 'Arias',                         ['brand'] = 'Bordeaux',        ['price'] = 23000,   ['categoryLabel'] = 'Compacts',        ['shop'] = 'pdm'}, --lorefriendly
+    {['model'] = 'eva',           ['name'] = 'EVA',                           ['brand'] = 'Hijak',           ['price'] = 23000,   ['categoryLabel'] = 'Compacts',        ['shop'] = 'pdm'}, --lorefriendly
+    {['model'] = 'hotwee',        ['name'] = 'Hotring Weevil',                ['brand'] = 'BF',              ['price'] = 23000,   ['categoryLabel'] = 'Compacts',        ['shop'] = 'pdm'}, --lorefriendly
+    {['model'] = 'peanut',        ['name'] = 'Peanut',                        ['brand'] = 'Weeny',           ['price'] = 23000,   ['categoryLabel'] = 'Compacts',        ['shop'] = 'pdm'}, --lorefriendly
+    --- Sedans (1)
+    {['model'] = 'dilettantedx',  ['name'] = 'Dilettante DX',                 ['brand'] = 'Karin',           ['price'] = 17000,   ['categoryLabel'] = 'Sedans',          ['shop'] = 'pdm'},     --lorefriendly
+    {['model'] = 'elegant',       ['name'] = 'Elegant',                       ['brand'] = 'Willard',         ['price'] = 17000,   ['categoryLabel'] = 'Sedans',          ['shop'] = 'pdm'},     --lorefriendly
+    {['model'] = 'elegyheritage', ['name'] = 'Elegy Heritage',                ['brand'] = 'Annis',           ['price'] = 17000,   ['categoryLabel'] = 'Sedans',          ['shop'] = 'pdm'},     --lorefriendly
+    {['model'] = 'glendalelimo',  ['name'] = 'Glendale Stretch',              ['brand'] = 'Karin',           ['price'] = 17000,   ['categoryLabel'] = 'Sedans',          ['shop'] = 'luxury'},  --lorefriendly
+    {['model'] = 'panorama',      ['name'] = 'Panorama',                      ['brand'] = 'Classique',       ['price'] = 17000,   ['categoryLabel'] = 'Sedans',          ['shop'] = 'pdm'},     --lorefriendly
+    --- SUVs (2)
+    {['model'] = 'atlas',         ['name'] = 'Atlas',                         ['brand'] = 'Karin',           ['price'] = 270000,  ['categoryLabel'] = 'SUVs',            ['shop'] = 'pdm'},--lorefriendly
+    {['model'] = 'executioner',   ['name'] = 'Executioner',                   ['brand'] = 'Vapid',           ['price'] = 270000,  ['categoryLabel'] = 'SUVs',            ['shop'] = 'pdm'},--lorefriendly
+    --- Coupes (3)
+    {['model'] = 'clique2',       ['name'] = 'Clique Deluxe',                 ['brand'] = 'Vapid',           ['price'] = 20000,   ['categoryLabel'] = 'Coupes',          ['shop'] = 'pdm'},--lorefriendly
+    {['model'] = 'paragonxr',     ['name'] = 'Paragon XR',                    ['brand'] = 'Enus',            ['price'] = 20000,   ['categoryLabel'] = 'Coupes',          ['shop'] = 'pdm'},--lorefriendly
+    {['model'] = 'sentinelsg4',   ['name'] = 'Sentinel SG4',                  ['brand'] = 'Übermacht',       ['price'] = 20000,   ['categoryLabel'] = 'Coupes',          ['shop'] = 'pdm'},--lorefriendly
+    {['model'] = 'stratumc',      ['name'] = 'Stratum',                       ['brand'] = 'Zircoflow',       ['price'] = 20000,   ['categoryLabel'] = 'Coupes',          ['shop'] = 'pdm'},--lorefriendly
+    {['model'] = 'streiter2',     ['name'] = 'Streiter',                      ['brand'] = 'Benefactor',      ['price'] = 20000,   ['categoryLabel'] = 'Coupes',          ['shop'] = 'pdm'},--lorefriendly
+    {['model'] = 'spritzer',      ['name'] = 'Spritzer STR',                  ['brand'] = 'Benefactor',      ['price'] = 20000,   ['categoryLabel'] = 'Coupes',          ['shop'] = 'pdm'},--lorefriendly
+    {['model'] = 'spritzerdtm',   ['name'] = 'Spritzer DTM',                  ['brand'] = 'Benefactor',      ['price'] = 20000,   ['categoryLabel'] = 'Coupes',          ['shop'] = 'pdm'},--lorefriendly
+    {['model'] = 'elegyrh7',      ['name'] = 'Elegy RH7',                     ['brand'] = 'Annis',           ['price'] = 124000,  ['categoryLabel'] = 'Coupes',          ['shop'] = 'pdm'},--lorefriendly
+    {['model'] = 'zr380c',        ['name'] = 'ZR-380',                        ['brand'] = 'Annis',           ['price'] = 55000,   ['categoryLabel'] = 'Coupes',          ['shop'] = 'luxury'},--lorefriendly 
+    {['model'] = 'estancia',      ['name'] = 'Estancia',                      ['brand'] = 'Ocelot',          ['price'] = 145000,   ['categoryLabel'] = 'Coupes',         ['shop'] = 'luxury'},--lorefriendly 
+    --- Muscle (4)
+    {['model'] = 'domc',          ['name'] = 'Dominator Classic',             ['brand'] = 'Vapid',           ['price'] = 105000,  ['categoryLabel'] = 'Muscle',          ['shop'] = 'pdm'},--lorefriendly
+    {['model'] = 'dominatorgtc',  ['name'] = 'Dominator GTC',                 ['brand'] = 'Vapid',           ['price'] = 105000,  ['categoryLabel'] = 'Muscle',          ['shop'] = 'pdm'},--lorefriendly
+    {['model'] = 'dukes4',        ['name'] = 'Dukes Cobra',                   ['brand'] = 'Imponte',         ['price'] = 105000,  ['categoryLabel'] = 'Muscle',          ['shop'] = 'pdm'},--lorefriendly
+    {['model'] = 'gauntletc',     ['name'] = 'Gauntlet Classic',              ['brand'] = 'Bravado',         ['price'] = 105000,  ['categoryLabel'] = 'Muscle',          ['shop'] = 'pdm'},--lorefriendly
+    {['model'] = 'gauntletstx',   ['name'] = 'Gauntlet STX',                  ['brand'] = 'Bravado',         ['price'] = 105000,  ['categoryLabel'] = 'Muscle',          ['shop'] = 'pdm'},--lorefriendly
+    {['model'] = 'paradox',       ['name'] = 'Paradox',                       ['brand'] = 'Willard',         ['price'] = 105000,  ['categoryLabel'] = 'Muscle',          ['shop'] = 'pdm'},--lorefriendly
+    {['model'] = 'paradox2',      ['name'] = 'Paradox',                       ['brand'] = 'Willard',         ['price'] = 105000,  ['categoryLabel'] = 'Muscle',          ['shop'] = 'pdm'},--lorefriendly
+    {['model'] = 'torido',        ['name'] = 'Torido',                        ['brand'] = 'Annis',           ['price'] = 105000,  ['categoryLabel'] = 'Muscle',          ['shop'] = 'pdm'},--lorefriendly
+    {['model'] = 'vulture',       ['name'] = 'Vulture',                       ['brand'] = 'Bravado',         ['price'] = 105000,  ['categoryLabel'] = 'Muscle',          ['shop'] = 'pdm'},--lorefriendly
+    --- Sports Classic (5)
+    {['model'] = 'cheetahfel',    ['name'] = 'Cheetah Veloce',                ['brand'] = 'Grotti',          ['price'] = 165000,  ['categoryLabel'] = 'Sports Classics', ['shop'] = 'luxury'},--lorefriendly
+    {['model'] = 'stardust',      ['name'] = 'Stardust',                      ['brand'] = 'Pfister',         ['price'] = 165000,  ['categoryLabel'] = 'Sports Classics', ['shop'] = 'luxury'},--lorefriendly
+    --- Sports (6)
+    {['model'] = 'buffalo4h',     ['name'] = 'Buffalo Hellfire',              ['brand'] = 'Bravado',         ['price'] = 55000,   ['categoryLabel'] = 'Sports',          ['shop'] = 'pdm'},--lorefriendly
+    {['model'] = 'coquette4c',    ['name'] = 'Coquette D10 Widebody',         ['brand'] = 'Invetero',        ['price'] = 55000,   ['categoryLabel'] = 'Sports',          ['shop'] = 'pdm'},--lorefriendly
+    {['model'] = 'elegyrace',     ['name'] = 'Elegy Race',                    ['brand'] = 'Annis',           ['price'] = 55000,   ['categoryLabel'] = 'Sports',          ['shop'] = 'pdm'},--lorefriendly
+    {['model'] = 'euros',         ['name'] = 'Euros',                         ['brand'] = 'Annis',           ['price'] = 55000,   ['categoryLabel'] = 'Sports',          ['shop'] = 'pdm'},--lorefriendly
+    {['model'] = 'f620s2',        ['name'] = 'F620 Sleeper',                  ['brand'] = 'Ocelot',          ['price'] = 55000,   ['categoryLabel'] = 'Sports',          ['shop'] = 'luxury'},--lorefriendly
+    {['model'] = 'italigts',      ['name'] = 'Itali GTS',                     ['brand'] = 'Grotti',          ['price'] = 55000,   ['categoryLabel'] = 'Sports',          ['shop'] = 'luxury'},--lorefriendly
+    {['model'] = 'meteor',        ['name'] = 'Meteor',                        ['brand'] = 'Pfister',         ['price'] = 55000,   ['categoryLabel'] = 'Sports',          ['shop'] = 'luxury'},--lorefriendly
+    {['model'] = 'kawaii',        ['name'] = 'Kawaii',                        ['brand'] = 'Annis',           ['price'] = 55000,   ['categoryLabel'] = 'Sports',          ['shop'] = 'luxury'},--lorefriendly
+    {['model'] = 'raidenz',       ['name'] = 'Raiden Z',                      ['brand'] = 'Coil',            ['price'] = 55000,   ['categoryLabel'] = 'Sports',          ['shop'] = 'pdm'},--lorefriendly
+    {['model'] = 'sheavas',       ['name'] = 'Sheava',                        ['brand'] = 'Emperor',         ['price'] = 55000,   ['categoryLabel'] = 'Sports',          ['shop'] = 'luxury'},--lorefriendly 
+    {['model'] = 'picadorexr',    ['name'] = 'Picador EXR',                   ['brand'] = 'Cheval',          ['price'] = 55000,   ['categoryLabel'] = 'Sports',          ['shop'] = 'luxury'},--lorefriendly 
+    {['model'] = 'roxanne',       ['name'] = 'Roxanne',                       ['brand'] = 'Annis',           ['price'] = 55000,   ['categoryLabel'] = 'Sports',          ['shop'] = 'luxury'},--lorefriendly 
+    {['model'] = 'sunrise1',      ['name'] = 'Sunrise R',                     ['brand'] = 'Maibatsu',        ['price'] = 55000,   ['categoryLabel'] = 'Sports',          ['shop'] = 'luxury'},--lorefriendly 
+    {['model'] = 'zion4',         ['name'] = 'Zion Classic Custom',           ['brand'] = 'Übermacht',       ['price'] = 55000,   ['categoryLabel'] = 'Sports',          ['shop'] = 'luxury'},--lorefriendly 
+    {['model'] = 'zr380c',        ['name'] = 'ZR-380',                        ['brand'] = 'Annis',           ['price'] = 55000,   ['categoryLabel'] = 'Sports',          ['shop'] = 'luxury'},--lorefriendly
+    {['model'] = 'zr380s',        ['name'] = 'ZR-380 Custom',                 ['brand'] = 'Annis',           ['price'] = 55000,   ['categoryLabel'] = 'Sports',          ['shop'] = 'luxury'},--lorefriendly
+
+    --- Super (7)
+
+    --- Motorcycles (8)
+    {['model'] = 'akumac',        ['name'] = 'Akuma Classic',                 ['brand'] = 'Dinka',           ['price'] = 7000,    ['categoryLabel'] = 'Motorcycles',     ['shop'] = 'pdm'},--lorefriendly
+    {['model'] = 'kampfer',       ['name'] = 'Kampfer',                       ['brand'] = 'Übermacht',       ['price'] = 7000,    ['categoryLabel'] = 'Motorcycles',     ['shop'] = 'pdm'},--lorefriendly
+    --- Off-Road (9)
+    {['model'] = 'baller7r',      ['name'] = 'Baller Raid',                   ['brand'] = 'Gallivanter',     ['price'] = 99000,   ['categoryLabel'] = 'Off Road',        ['shop'] = 'pdm'},--lorefriendly
+    {['model'] = 'blazer6',       ['name'] = 'Blazer Recon ATV',              ['brand'] = 'Nagasaki',        ['price'] = 99000,   ['categoryLabel'] = 'Off Road',        ['shop'] = 'pdm'},--lorefriendly
+    {['model'] = 'gstyosemite1',  ['name'] = 'Yosemite DRT',                  ['brand'] = 'Declasse',        ['price'] = 99000,   ['categoryLabel'] = 'Off Road',        ['shop'] = 'pdm'},--lorefriendly
+    {['model'] = 'squaddier',     ['name'] = 'Squaddier',                     ['brand'] = 'Mammoth',         ['price'] = 99000,   ['categoryLabel'] = 'Off Road',        ['shop'] = 'pdm'},--lorefriendly
+    {['model'] = 'vigout',        ['name'] = 'Vigero Outlaw',                 ['brand'] = 'Declasse',        ['price'] = 99000,   ['categoryLabel'] = 'Off Road',        ['shop'] = 'pdm'},--lorefriendly
+    --- Industrial (10)
+    
+    --- Utility (11)
+    {['model'] = 'airtug',        ['name'] = 'Airtug',                        ['brand'] = 'HVY',             ['price'] = 30000,   ['categoryLabel'] = 'Utility',         ['shop'] = 'truck'},--lorefriendly
+    {['model'] = 'benson2',       ['name'] = 'Benson',                        ['brand'] = 'Vapid',           ['price'] = 30000,   ['categoryLabel'] = 'Utility',         ['shop'] = 'pdm'},  --lorefriendly
+    --- Vans (12)
+    {['model'] = 'burritor',      ['name'] = 'Burrito Racing',                ['brand'] = 'Declasse',        ['price'] = 45000,   ['categoryLabel'] = 'Vans',            ['shop'] = 'pdm'},--lorefriendly
+    {['model'] = 'nspeedo',       ['name'] = 'Speedo Express',                ['brand'] = 'Vapid',           ['price'] = 45000,   ['categoryLabel'] = 'Vans',            ['shop'] = 'pdm'},--lorefriendly
+    {['model'] = 'relaxer',       ['name'] = 'Relaxer',                       ['brand'] = 'Brute',           ['price'] = 45000,   ['categoryLabel'] = 'Vans',            ['shop'] = 'pdm'},--lorefriendly
+    --- Cycles (13)
+    --- Boats (14)
+    --- Helicopters (15)
+    --- Planes (16)
+    --- Service (17)
+    --- Emergency (18)
+    --- Military (19)
+    --- Commercial (20)
+    --- Trains (21)
+    --- Open Wheel (22)
